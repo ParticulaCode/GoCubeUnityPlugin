9ec5481142eca084874654aa32076ca63c32c295
Particula <58478137+ParticulaCode@users.noreply.github.com>
2020-02-27 17:19:08 +0200

    Add files via upload
    Add iOS required permission post build (BT permission)

---
+++ Assets/Editor/iOS_AddPermission.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+using System.IO;
+using System.Collections.Generic;
+
+public class CloudBuildHelper : MonoBehaviour
+{
+#if UNITY_IOS
+    [PostProcessBuild]
+    static void OnPostprocessBuild(BuildTarget buildTarget, string path)
+    {
+        // Read plist
+        var plistPath = Path.Combine(path, "Info.plist");
+        var plist = new PlistDocument();
+        plist.ReadFromFile(plistPath);
+ 
+        // Update value
+        PlistElementDict rootDict = plist.root;
+        rootDict.SetString("NSBluetoothPeripheralUsageDescription", "Used to connect to the cube");
+ 
+        // Write plist
+        File.WriteAllText(plistPath, plist.WriteToString());
+    }
+#endif
+}
